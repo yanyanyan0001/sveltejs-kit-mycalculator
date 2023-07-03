717453883f5cc119105c84b914e71ef7e3fe7520
yanyanyan0001 <106573585+yanyanyan0001@users.noreply.github.com>
2023-07-03 20:49:41 +0800

    Initial commit

---
+++ src/routes/api/calculate.ts
@@ -0,0 +1,13 @@
+export async function post(request) {
+    const { expression } = JSON.parse(request.body);
+      let result;
+try {
+  result = eval(expression); 
+} 
+catch (error) {
+        result = 'Error';
+      }
+  return {
+          body: JSON.stringify({ result }),
+        };
+}
